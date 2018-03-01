6aea7567c7f8ffe7d0ea19c3983012251d0d3610
Jaleesa McCrea <jaleesa@Jaleesas-MacBook-Pro.local>
2018-03-01 14:12:34 -0500

    added more stuff

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,5 @@
 
+var counter = 0;
 
 
 //computer picks random # between 19-120 and displays on screen at game start
@@ -9,8 +10,8 @@ $( "#rand" ).append( document.createTextNode( compP ) );
 
 
 //computer randomly assigns each of the 4 crystals a # between 1-12
-//When player clicks gem numbers add up together in own divx
-//TO DO: make numbers no dduplicates
+//When player clicks gem numbers add up together in own div
+
 
 green = Math.floor(Math.random() * 12 + 1);
 console.log(green);
@@ -18,40 +19,65 @@ $("#green").click(function(g){
     alert(green);
 });
 
-//TO DO: make number sum of button pushes add together
+
+//makes number sum of button pushes add together
     $(document).ready(function() {
 
-        $("#green").click(function(){
-            var counter = green;
-            counter+=
-            $("#green").val(green);
-            $("#theCounter").text(counter);
+        $(".crystal").click(function(){    
+            // counter += green;
+            //  $("#green").val(green);
+            
+            //  counter += purple;
+            //  $("#purple").val(purple );
+
+
+            //TODO: If green button is pushed
+            
+           
+                counter += green;
+                $("#green").val(green);
+                $("#theCounter").text(counter);
+           
+            
+           
+                counter += purple;
+                $("#purple").val(purple);
+                $("#theCounter").text(counter);
+            
+            
+                counter += blue;
+                $("#blue").val(blue);
+                $("#theCounter").text(counter);
+
+                counter += gold;
+                $("#gold").val(gold);
+                $("#theCounter").text(counter);
+        
+            console.log(green);
+            console.log(purple);
+            console.log(blue);
+            console.log(gold);
+             $("#theCounter").text(counter);
+
         });
 
-    
+purple = Math.floor(Math.random() * 12 + 1);
+console.log(purple);
+$("#purple").click(function(p){ 
+    alert(purple);
+});
+   
+blue = Math.floor(Math.random() * 12 + 1);
+console.log(blue);
+$("#blue").click(function(b){ 
+    alert(blue);
+});
 
-
-
-
-
-// purple = Math.floor(Math.random() * 12 + 1);
-// console.log(purple);
-// $("#purple").click(function(p){ 
-//     alert(purple);
-// });
-
-
-// blue = Math.floor(Math.random() * 12 + 1);
-// console.log(blue);
-// $("#blue").click(function(b){ 
-//     alert(blue);
-// });
-
-// gold = Math.floor(Math.random() * 12 + 1);
-// console.log(gold);
-// $("#gold").click(function(a){ 
-//     alert(gold);
-// });
+gold = Math.floor(Math.random() * 12 + 1);
+console.log(gold);
+$("#gold").click(function(a){ 
+    alert(gold);
+});
 
 //player clicks on the crystals and the random number value accumulates and appears on screen.
 // if the "crystal total" number === computer pick number screen prints "you won" 
